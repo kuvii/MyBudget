1878f0ddb6f7658983476d9bab945f238cf4df45
kuvii <kuvilet@gmail.com>
2022-11-16 13:54:17 +0000

    Feat(App): Add transaction lists

--- App.js
+++ App.js
@@ -11,11 +11,14 @@ export default function App() {
   const [show, setShow] = useState(true)
 
   const [totalBalance, setTotalBalance] = useState(0)
-  const [list, setList] = useState([])
+  const [entryList, setEntryList] = useState([])
+  const [spentList, setSpentList] = useState([])
 
-  const addTransaction = (newTransaction) => {
-    setList([...list, newTransaction])
-  }
+
+
+  const entry = <ScrollView style={styles.entryPanel}>{entryList.map(() => (<View style={styles.transaction}><Text>{transaction.date}</Text></View>))}</ScrollView>
+  const spent = <ScrollView style={styles.spentPanel}>{spentList.map(() => (<View style={styles.transaction}><Text>{transaction.date}</Text></View>))}</ScrollView>
+  
   return (
     <View style={styles.container}>
 
@@ -23,6 +26,17 @@ export default function App() {
         <Text style={styles.actualBalance}>Saldo Actual:</Text>
         <Text style={styles.amount} >{totalBalance}</Text>
       </View>
+
+      <View style={styles.transactionList}>
+        <View style={styles.selector}>
+          <Pressable onPress={() => setShow(true)} style={[styles.selectionStyle, styles.entryColor, styles.btl10]}/>
+          <Pressable onPress={() => setShow(false)} style={[styles.selectionStyle, styles.spentColor, styles.btr10]}/>
+        </View>
+        
+        {show ? entry : spent}
+        
+      </View>
+
 
       
     </View>
@@ -53,4 +67,58 @@ const styles = StyleSheet.create({
     margin: 20,
     fontSize: 40,
   },
+  selector: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    height: '20%',
+    width: '100%',
+  },
+  transactionList: {
+    borderRadius: 10,
+    height: '50%',
+    margin: 50,
+    width: '90%',
+  },
+  selectionStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '50%',
+  },
+  entryPanel: {
+    backgroundColor: "#51D190",
+    width: '100%',
+    height: '100%',
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  spentPanel: {
+    backgroundColor: "#DA6590",
+    width: '100%',
+    height: '80%',
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  entryColor: {
+    backgroundColor: "#51D175"
+  },
+  spentColor: {
+    backgroundColor: "#DA6565"
+  },
+  btl10: {
+    borderTopLeftRadius: 10,
+  },
+  btr10: {
+    borderTopRightRadius: 10,
+  },
+  transaction: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    height: 50,
+    justifyContent: 'center',
+    marginTop: 10,
+    width: '80%',
+  }
 });
