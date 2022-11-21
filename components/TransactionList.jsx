b945efd246713f7e10c31f730d69ad23d90306a0
kuvii <kuvilet@gmail.com>
2022-11-21 23:56:42 +0000

    Refactor(TransactionInput): Create New Component

--- components/TransactionList.jsx
+++ components/TransactionList.jsx
@@ -1,44 +1,23 @@
 import React from "react";
 import { useState } from "react";
 import {
-  Button,
   Pressable,
   StyleSheet,
   Text,
-  Modal,
-  TextInput,
   View,
-  FlatList,
 } from "react-native";
-import DatePicker from "react-native-modern-datepicker";
+
 import  ShowList  from './ShowList'
+import TransactionInput from './TransactionInput'
 
 const TransactionList = ({ transaction, setTransaction, setTotalBalance }) => {
   const [show, setShow] = useState(true)
   const [entryList, setEntryList] = useState([])
   const [spentList, setSpentList] = useState([])
   const [showModal, setShowModal] = useState(false)
-  const [selectedDate, setSelectedDate] = useState("")
 
   const openModal = () => {
     setShowModal(!showModal)
-  }
-  const changeDescriptionHandle = (value) => {
-    setTransaction({ ...transaction, description: value })
-  }
-  const changeAmountHandle = (value) => {
-    setTransaction({ ...transaction, amount: value })
-  }
-  const changeDateHandle = (value) => {
-    setTransaction({...transaction, date: value})
-  }
-
-  const submitHandle = () => {
-    const obj = { ...transaction }
-    transaction.amount >= 0
-      ? setEntryList([...entryList, obj])
-      : setSpentList([...spentList, obj])
-    setTransaction({ ...transaction, amount: 0, description: "", date: "" })
   }
 
   const calculateTotalBalance = () => {
@@ -64,43 +43,26 @@ const TransactionList = ({ transaction, setTransaction, setTotalBalance }) => {
       <View style={styles.selector}>
         <Pressable
           onPress={() => setShow(true)}
-          style={[styles.selectionStyle, styles.entryColor, styles.btl10]}
+          style={styles.entrySelection}
         />
         <Pressable
           onPress={() => setShow(false)}
-          style={[styles.selectionStyle, styles.spentColor, styles.btr10]}
+          style={styles.spentSelection}
         />
       </View>
 
       {show ? <ShowList list={entryList} type="e"/> : <ShowList list={spentList} type="s"/>}
 
-      <Modal
-        animationType="fade"
-        visible={showModal}
-        onRequestClose={() => setShowModal(!showModal)}
-      >
-        <View style={styles.modalStyle}>
-          <Text>Nueva Transaccion</Text>
-          <TextInput
-            placeholder="Cantidad"
-            keyboardType="number-pad"
-            onChangeText={changeAmountHandle}
-            value={transaction.amount}
-          />
-          <TextInput
-            placeholder="Descripcion"
-            keyboardType="default"
-            onChangeText={changeDescriptionHandle}
-            value={transaction.description}
-          />
-          <DatePicker 
-          onSelectedChange={date =>  setSelectedDate(date)}
-          onDateChange={changeDateHandle}
-          value={transaction.date}
-          />
-          <Button title="submit" onPress={submitHandle} />
-        </View>
-      </Modal>
+      <TransactionInput 
+      transaction={transaction} 
+      setTransaction={setTransaction} 
+      showModal={showModal} 
+      setShowModal={setShowModal} 
+      entryList={entryList}
+      setEntryList={setEntryList}
+      spentList={spentList}
+      setSpentList={setSpentList}
+      />
 
       <Pressable onPress={() => openModal()}>
         <View style={styles.addTransactionButtonStyle}>
@@ -127,41 +89,22 @@ const styles = StyleSheet.create({
     height: 60,
     width: "100%",
   },
-  selectionStyle: {
+  entrySelection:{
     alignItems: "center",
+    backgroundColor: "#51D175",
+    borderTopLeftRadius: 10,
     justifyContent: "center",
     height: "100%",
     width: "50%",
   },
-  entryColor: {
-    backgroundColor: "#51D175",
-  },
-  spentColor: {
-    backgroundColor: "#DA6565",
-  },
-  btl10: {
-    borderTopLeftRadius: 10,
-  },
-  btr10: {
-    borderTopRightRadius: 10,
-  },
-  modalStyle: {
+spentSelection:{
     alignItems: "center",
-    justifyContent: "space-evenly",
-    backgroundColor: "white",
-    borderRadius: 20,
-    height: 470,
-    margin: 20,
-    padding: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
+    backgroundColor: "#DA6565",
+    borderTopRightRadius: 10,
+    justifyContent: "center",
+    height: "100%",
+    width: "50%",
+},
   addTransactionButtonStyle: {
     alignItems: "center",
     backgroundColor: "white",
